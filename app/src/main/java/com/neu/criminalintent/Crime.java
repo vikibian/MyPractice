e8a41db13ef80a786ba6b635f7accaf90901a046
VIKI-BIAN <710256138@qq.com>
2019-12-03 21:56:52 +0800

    创建了Crime类

---
+++ app/src/main/java/com/neu/criminalintent/Crime.java
@@ -0,0 +1,13 @@
+package com.neu.criminalintent;
+
+import java.util.UUID;
+
+public class Crime {
+    private UUID mId;
+    private String mTite;
+
+    public Crime(){
+        //Generate unique identifier
+        mId = UUID.randomUUID();
+    }
+}
